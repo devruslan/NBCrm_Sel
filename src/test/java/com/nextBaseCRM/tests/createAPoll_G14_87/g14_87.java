46bad48dccc74c0290e68cad26fd793c989ef907
Ruslan <ruslan.nasirov@gmail.com>
2021-01-31 12:07:58 -0500

    Initial Commit Added Packages Added g14_87 file Created tests some steps Created utility package Created Sleep class and Second method

--- src/test/java/com/nextBaseCRM/tests/createAPoll_G14_87/g14_87.java
+++ src/test/java/com/nextBaseCRM/tests/createAPoll_G14_87/g14_87.java
@@ -1,6 +1,7 @@
 package com.nextBaseCRM.tests.createAPoll_G14_87;
 
 import com.nextBaseCRM.tests.utilities.Sleep;
+import com.sun.tools.javac.comp.Check;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -8,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class g14_87 {
@@ -16,10 +18,9 @@ public class g14_87 {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 
         driver.get("https://login2.nextbasecrm.com");
-//
 
         WebElement userNameInput = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
         userNameInput.sendKeys("helpdesk27@cybertekschool.com");
@@ -34,38 +35,73 @@ public class g14_87 {
         String actualTitle = driver.getTitle();
         String expectedTitle = "Portal";
 
-        if ( actualTitle.endsWith(expectedTitle) ) System.out.println("PASSED! Expected: Title ends with Portal");
-        else System.out.println("FAILED!!! Expected: Title ends with Portal");
+        if ( actualTitle.endsWith(expectedTitle) )
+            System.out.println("PASSED! Expected: Title ends with 'Portal'");
+        else
+            System.out.println("FAILED!!! Expected: Title ends with 'Portal'");
+
         System.out.println("---------");
+
+//      Check if poll block exists before clicking POLL tab
+//        Sleep.seconds(2);
+//        List<WebElement> pollBlocksBeforeClick = driver.findElements(By.xpath("//form[@id='blogPostForm']"));
+//        if ( pollBlocksBeforeClick.size() == 0 )
+//            System.out.println("PASSED! Expected: Poll Block is NOT displayed BEFORE clicking POLL.");
+//        else
+//            System.out.println("FAILED!!! Expected: Poll Block is NOT displayed BEFORE clicking POLL.");
 
 //      Step 2
-//      Click Poll
-        driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-vote']")).click();
+//      Click Poll tab
+//        Sleep.seconds(2);
+        WebElement pollTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-vote']"));
+        pollTab.click();
+
 //      To be able to observe/view an empty/new Poll creation block.
-        Sleep.seconds(2);
-
-        WebElement topicInput = driver.findElement(By.xpath("//input[@id='POST_TITLE']"));
-        WebElement iframeInput = driver.findElement(By.xpath("//iframe[@class='bx-editor-iframe']"));
-        WebElement questionInput = driver.findElement(By.xpath("//input[@id='question_0']"));
-        WebElement answer_1_1_Input = driver.findElement(By.xpath("//input[@id='answer_0__0_']"));
-        WebElement sendButton = driver.findElement(By.xpath("//button[@id='blog-submit-button-save']"));
-        WebElement cancelLink = driver.findElement(By.xpath("//button[@id='blog-submit-button-cancel']"));
-
-        if ( topicInput.isDisplayed() && iframeInput.isDisplayed() &&
-                questionInput.isDisplayed() && answer_1_1_Input.isDisplayed() &&
-                sendButton.isDisplayed() && cancelLink.isDisplayed() )
-            System.out.println("PASSED! Expected: All elements are displayed.");
+//        Sleep.seconds(2);
+        List<WebElement> pollBlocksAfterClick = driver.findElements(By.xpath("//form[@id='blogPostForm']"));
+        if ( pollBlocksAfterClick.size() == 1 )
+            System.out.println("PASSED! Expected: Poll Block IS displayed AFTER clicking POLL.");
         else
-            System.out.println("FAILED!!! Expected: All elements are displayed.");
+            System.out.println("FAILED!!! Expected: Poll Block IS displayed AFTER clicking POLL.");
+
+
+//        WebElement pollBlock = driver.findElement(By.xpath("//form[@id='blogPostForm']"));
+//        if ( pollBlock.isDisplayed() ) System.out.println("Poll Block IS displayed.");
+//        else System.out.println("Poll Block is NOT displayed.");
+
+        WebElement topicInput       = driver.findElement(By.xpath("//input[@id='POST_TITLE']"));
+        WebElement iFrameMessage    = driver.findElement(By.xpath("//iframe[@class='bx-editor-iframe']"));
+        WebElement questionInput    = driver.findElement(By.xpath("//input[@id='question_0']"));
+        WebElement answer_1_1_Input = driver.findElement(By.xpath("//input[@id='answer_0__0_']"));
+        WebElement sendButton       = driver.findElement(By.xpath("//button[@id='blog-submit-button-save']"));
+        WebElement cancelLink       = driver.findElement(By.xpath("//button[@id='blog-submit-button-cancel']"));
+
+        System.out.println();
+        System.out.println("\t\ttopicInput is displayed:\t\t" +     topicInput      .isDisplayed());
+        System.out.println("\t\tiFrameMessage is displayed:\t\t" +     iFrameMessage     .isDisplayed());
+        System.out.println("\t\tquestionInput is displayed:\t\t" +  questionInput   .isDisplayed());
+        System.out.println("\t\tanswer_1_1_Input is displayed:\t" + answer_1_1_Input.isDisplayed());
+        System.out.println("\t\tsendButton is displayed:\t\t" +     sendButton      .isDisplayed());
+        System.out.println("\t\tcancelLink is displayed:\t\t" +     cancelLink      .isDisplayed());
+
+        if (topicInput          .isDisplayed()  &&
+            iFrameMessage       .isDisplayed()  &&
+            questionInput       .isDisplayed()  &&
+            answer_1_1_Input    .isDisplayed()  &&
+            sendButton          .isDisplayed()  &&
+            cancelLink          .isDisplayed()    )
+            System.out.println("PASSED! Expected: All poll elements are displayed.");
+        else
+            System.out.println("FAILED!!! Expected: All poll elements are displayed.");
 
         System.out.println("---------");
-
 
 //      Step 3
 //      Click the "Topic" text field
 //      Topic text field is enabled
         if ( topicInput.isEnabled() ) System.out.println("PASSED! Expected: Topic is enabled");
         else System.out.println("FAILED!!! Expected: Topic is enabled");
+
         System.out.println("---------");
 
 //      Step 5
@@ -73,6 +109,7 @@ public class g14_87 {
 //      Question text field is enabled
         if ( questionInput.isEnabled() ) System.out.println("PASSED! Expected: Question is enabled");
         else System.out.println("FAILED!!! Expected: Question is enabled");
+
         System.out.println("---------");
 
 //      Step 7
@@ -80,6 +117,7 @@ public class g14_87 {
 //      Answer text field is enabled
         if ( answer_1_1_Input.isEnabled() ) System.out.println("PASSED! Expected: Answer is enabled");
         else System.out.println("FAILED!!! Expected: Answer is enabled");
+
         System.out.println("---------");
 
 //      Step 9
@@ -119,9 +157,9 @@ public class g14_87 {
         WebElement answer_2_1_Input = driver.findElement(By.xpath("//input[@id='answer_1__0_']"));
         WebElement pollCheckBox2 = driver.findElement(By.xpath("//input[@id='multi_1']"));
 
-        if ( question2Input.isDisplayed() &&
+        if (    question2Input  .isDisplayed() &&
                 answer_2_1_Input.isDisplayed() &&
-                pollCheckBox2.isDisplayed() )
+                pollCheckBox2   .isDisplayed() )
             System.out.println("PASSED! Expected: Second set of Q/A appended");
         else
             System.out.println("FAILED!!! Expected: Second set of Q/A appended");
@@ -130,34 +168,95 @@ public class g14_87 {
 
 //      Step 11
 //      Click "Cancel" link
+        Sleep.seconds(1);
+        cancelLink = driver.findElement(By.xpath("//button[@id='blog-submit-button-cancel']"));
+        cancelLink.click();
+
+
 //      The poll should be deleted.
 //      The data should be deleted.
 //      The Messages tab should be selected.
+        Sleep.seconds(1);
+
+        WebElement pollBlock = driver.findElement(By.xpath("//form[@id='blogPostForm']"));
+        if ( pollBlock.isDisplayed() ) System.out.println("FAILED!!! Expected: Poll Block is NOT displayed.");
+        else System.out.println("PASSED! Expected: Poll Block is NOT displayed.");
+
+        pollBlocksAfterClick = driver.findElements(By.xpath("//form[@id='blogPostForm']"));
+        if ( pollBlocksAfterClick.size() == 0 && !pollBlock.isDisplayed())
+            System.out.println("PASSED! Expected: Poll Block is REMOVED after clicking CANCEL.");
+        else if ( pollBlocksAfterClick.size() != 0 && !pollBlock.isDisplayed()) {
+            System.out.println("FAILED!!!\tExpected: Poll Block is REMOVED after clicking CANCEL.");
+            System.out.println("\t\t\tActual: Poll block is hidden but not removed.");
+        } else if ( pollBlocksAfterClick.size() != 0 && pollBlock.isDisplayed()) {
+            System.out.println("FAILED!!! Expected: Poll Block is REMOVED after clicking CANCEL.");
+            System.out.println("\t\t\tActual: Poll block is displayed and is not removed.");
+        }
+        System.out.println("---------");
+
+//      Open poll again in order to test the SEND button and poll confirmation
+        Sleep.seconds(1);
+        pollTab.click();
+
+//      Step 4
+//      Topic: Input a string (Upper case letters, lowercase letters, numbers, special chars)
+//      "aA@2"
+//      Input field should accept all character types
+        Sleep.seconds(1);
+        topicInput.sendKeys("aA@2 1");
+
+//      Step 4
+//      Message: Input a string (Upper case letters, lowercase letters, numbers, special chars)
+//      "aA@2"
+//      Input field should accept all character types
+        Sleep.seconds(1);
+        driver.switchTo().frame(iFrameMessage);
+        WebElement iframeMessageBody = driver.findElement(By.xpath("//body[@contenteditable='true']"));
+        iframeMessageBody.sendKeys("aA@2 2");
+        driver.switchTo().defaultContent();
+
+//      Step 6
+//      Question: Input a string (Upper case letters, lowercase letters, numbers, special chars)
+//      "aA@2"
+//      Input field should accept all character types
+        Sleep.seconds(1);
+        questionInput.sendKeys("aA@2 3");
+
+//      Step 8
+//      Answer: Input a string (Upper case letters, lowercase letters, space, numbers, special chars)
+//      "aA@2"
+//      Input field should accept all character types
+        Sleep.seconds(1);
+        answer_1_1_Input.sendKeys("aA@2 4");
+
 
 //      Step 12
 //      Click "SEND" button
 //      The poll should be posted on the Activity Stream page, on the top.
 //      I should be able to see all my data.
+        Sleep.seconds(1);
+        sendButton = driver.findElement(By.xpath("//button[@id='blog-submit-button-save']"));
+        sendButton.click();
 
-//      Step 4
-//      Topic: Input a string (Upper case letters, lowercase letters, space, numbers, special chars)
-//      "Lorem ipsum dolor sit amen       LOREM IMSUM DOLOR SIT AMEN 0123456789 ~!@#$%^&*()_+=-`<>?/|\"
-//      I should be able to input all the characters of the data text
-//
-//      Step 6
-//      Question: Input a string (Upper case letters, lowercase letters, space, numbers, special chars)
-//      "Lorem ipsum dolor sit amen       LOREM IMSUM DOLOR SIT AMEN 0123456789 ~!@#$%^&*()_+=-`<>?/|\"
-//      I should be able to input all the characters of the data text
-//
-//      Step 8
-//      Answer: Input a string (Upper case letters, lowercase letters, space, numbers, special chars)
-//      "Lorem ipsum dolor sit amen       LOREM IMSUM DOLOR SIT AMEN 0123456789 ~!@#$%^&*()_+=-`<>?/|\"
-//      I should be able to input all the characters of the data text
+//      Verify that the Poll is posted by verifying the given data is displayed
+        Sleep.seconds(1);
+        WebElement posted_PollTopic = driver.findElement(By.xpath("//div[@class='feed-post-item']"));
+        WebElement posted_PollMessage = driver.findElement(By.xpath("//div[@class='feed-post-text-block-inner-inner']"));
+        WebElement posted_PollQuestion = driver.findElement(By.xpath("//div[@class='bx-vote-question-title']"));
+        WebElement posted_PollAnswer = driver.findElement(By.xpath("//div[@class='bx-vote-bar']"));
+
+        String expectedText = "aA@2";
+        if (    posted_PollTopic.getText().contains(expectedText)     &&
+                posted_PollMessage.getText().contains(expectedText)   &&
+                posted_PollQuestion.getText().contains(expectedText)  &&
+                posted_PollAnswer.getText().contains(expectedText)    )
+            System.out.println("PASSED! Expected: Data is correctly displayed. Poll successfully posted.");
+        else
+            System.out.println("FAILED!!! Expected: Data is correctly displayed. Poll successfully posted.");
 
 
-        Sleep.seconds(2);
+        Sleep.seconds(3);
         driver.close();
-
 
     }
 }
