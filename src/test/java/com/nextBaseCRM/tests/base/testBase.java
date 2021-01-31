1dce5fba62bb95aadfd2a696a93d71d8ad28ebf7
Ruslan <ruslan.nasirov@gmail.com>
2021-01-31 16:12:41 -0500

    Created the ConfigurationReader code

--- src/test/java/com/nextBaseCRM/tests/base/testBase.java
+++ src/test/java/com/nextBaseCRM/tests/base/testBase.java
@@ -1,9 +1,23 @@
 package com.nextBaseCRM.tests.base;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
 
 public class testBase {
 
+    public WebDriver driver;
 
+    public void login(){
+
+        WebElement userNameInput = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        userNameInput.sendKeys("");
+
+        WebElement passwordInput = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        passwordInput.sendKeys("" + Keys.ENTER);
+
+    }
 
 }
